4f9fb10286e798295531781a908656133ed479dc
İrem Rakıcı <iremrakici53@hotmail.com>
2024-10-22 11:21:36 +0300

    Proje dosyası ekle.

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,65 @@
+﻿namespace Polymorphism
+{
+    public class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        public BaseGeometrikSekil(double _genislik, double _yukseklik)
+        {
+            Genislik = _genislik;
+            Yukseklik = _yukseklik;
+        }
+
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare(double _genislik, double _yukseklik) : base(_genislik, _yukseklik)
+        {
+            Genislik = _genislik;
+            Yukseklik = _yukseklik;
+            Console.WriteLine($"Karenin alanı:{AlanHesapla()}");
+        }
+
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+    }
+
+    public class Dıkdortgen : BaseGeometrikSekil
+    {
+        public Dıkdortgen(double _genislik, double _yukseklik) : base(_genislik, _yukseklik)
+        {
+            Genislik = _genislik;
+            Yukseklik = _yukseklik;
+            Console.WriteLine($"Dikdörtgenin alanı:{AlanHesapla()}");
+        }
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+    }
+
+    public class Ucgen : BaseGeometrikSekil
+    {
+        public Ucgen(double taban, double yukseklik) : base(taban, yukseklik)
+        {
+            Genislik = taban;
+            Yukseklik = yukseklik;
+
+            Console.WriteLine($"Üçgenin alanı:{AlanHesapla()}");
+
+        }
+        public override double AlanHesapla()
+        {
+            return (Genislik * Yukseklik) / 2;
+
+        }
+    }
+
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using Polymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BaseGeometrikSekil ucgen = new Ucgen(18, 20);// BaseGeometrikSekil türünde bir üçgen nesnesi oluşturuluyor.
+        ucgen.AlanHesapla();
+
+        Console.WriteLine("------------------------------------------");
+
+        BaseGeometrikSekil kare = new Kare(14, 16);// BaseGeometrikSekil türünde bir kare nesnesi oluşturuluyor.
+        kare.AlanHesapla();
+
+        Console.WriteLine("------------------------------------------");// BaseGeometrikSekil türünde bir dikdörtgen nesnesi oluşturuluyor.
+
+        BaseGeometrikSekil dıkdortgen = new Dıkdortgen(20, 30);
+        dıkdortgen.AlanHesapla();
+
+    }
+}
